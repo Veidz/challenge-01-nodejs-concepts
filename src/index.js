4a285deed683ef130ba075fd021c9bfb90667abc
Veidz <joaovictorveidz@gmail.com>
2022-09-10 20:49:44 -0300

    feat: ensure it is possible to complete a task

--- src/index.js
+++ src/index.js
@@ -98,7 +98,19 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params
+  const { username } = request.headers
+
+  const user = users.find((user) => user.username === username)
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({ error: 'No task found' })
+  }
+
+  todo.done = true
+
+  return response.status(200).json(todo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
